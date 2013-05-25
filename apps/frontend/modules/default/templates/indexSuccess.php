11525d786b9edaa936fd7f4db6999f391a15916f
Anatoly Pashin <anatoly.pashin@gmail.com>
2013-05-26 05:58:45 +1100

    some layout and sf

--- apps/frontend/modules/default/templates/indexSuccess.php
+++ apps/frontend/modules/default/templates/indexSuccess.php
@@ -1,24 +1,85 @@
-<div class="page-header">
-  <h1>Symfony Project Created</h1>
-  <p class="lead">Congratulations! You have successfully created your symfony project.</p>
+<div class="text-center"> <!-- http://stackoverflow.com/questions/7165423/twitter-bootstrap-center-pills -->
+  <ul class="nav nav-pills" style="display:inline-block">
+    <li class="active"><a href="">Все</a></li>
+    <li><a href="">Красота</a></li>
+    <li><a href="">Здоровье</a></li>
+    <li><a href="">Развлечения</a></li>
+    <li><a href="">Спорт</a></li>
+    <li><a href="">Обучение</a></li>
+    <li><a href="">Авто</a></li>
+    <li><a href="">Отдых</a></li>
+  </ul>
 </div>
 
+<div class="mega-cells">
+  <div class="row">
+    <div class="span6">
+      <div class="description">
+        <div class="description-full">
+          <div class="name">Механическая и ультразвуковая чистка лица, омолаживающий массаж лица с альгинатной фруктовой маской, 4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
+          <div class="counters">
+            <span class="eta">3 дня 23:12</span>
+            <span class="comments">234</span>
+            <span class="likes">71</span>
+          </div>
+        </div>
 
-<dl>
-  <dt>Project setup successful</dt>
-  <dd>This project uses the symfony libraries. If you see no image in this page, you may need to configure your web server so that it gains access to the <code>symfony_data/web/sf/</code> directory.</dd>
-</dl>
-<dl>
-  <dt>This is a temporary page</dt>
-  <dd>This page is part of the symfony <code>default</code> module. It will disappear as soon as you define a <code>homepage</code> route in your <code>routing.yml</code>.</dd>
-</dl>
-<dl>
-  <dt>What's next</dt>
-  <dd>
-    <ul>
-      <li>Create your data model</li>
-      <li>Customize the layout of the generated templates</li>
-      <li><?php echo link_to('Learn more from the online documentation', 'http://www.symfony-project.org/doc') ?></li>
-    </ul>
-  </dd>
-</dl>
+        <div class="description-short">
+          <div class="name">4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
+        </div>
+      </div>
+
+      <div class="buy-me-faster">
+        право на скидку 70%
+        50 руб.
+        <a href="" class="btn">Купить</a>
+      </div>
+    </div>
+
+    <div class="span6">
+      <div class="description">
+        <div class="description-full">
+          <div class="name">Механическая и ультразвуковая чистка лица, омолаживающий массаж лица с альгинатной фруктовой маской, 4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
+          <div class="counters">
+            <span class="eta">3 дня 23:12</span>
+            <span class="comments">234</span>
+            <span class="likes">71</span>
+          </div>
+        </div>
+
+        <div class="description-short">
+          <div class="name">4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
+        </div>
+      </div>
+
+      <div class="buy-me-faster">
+        право на скидку 70%
+        50 руб.
+        <a href="" class="btn">Купить</a>
+      </div>
+    </div>
+
+    <div class="span6">
+      <div class="description">
+        <div class="description-full">
+          <div class="name">Механическая и ультразвуковая чистка лица, омолаживающий массаж лица с альгинатной фруктовой маской, 4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
+          <div class="counters">
+            <span class="eta">3 дня 23:12</span>
+            <span class="comments">234</span>
+            <span class="likes">71</span>
+          </div>
+        </div>
+
+        <div class="description-short">
+          <div class="name">4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
+        </div>
+      </div>
+
+      <div class="buy-me-faster">
+        право на скидку 70%
+        50 руб.
+        <a href="" class="btn">Купить</a>
+      </div>
+    </div>
+  </div>
+</div>
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -14,45 +14,109 @@
 </head>
 
 <body>
-  <div class="navbar">
-    <div class="navbar-inner">
-      <div class="container">
-        <a href="<?php echo url_for('@homepage') ?>" class="brand"><?php echo $_SERVER['SERVER_NAME'] ?></a>
+  <div id="header" class="container">
+    <div class="login-bar hide">
+      <form action="" class="form-inline">
+        <input type="email" name="" id="" placeholder="Эл. почта" />
+        <input type="password" name="" id="" placeholder="Пароль" />
+        <button type="submit" class="btn">Войти</button>
+        <a href="">Регистрация</a>
+      </form>
 
-        <ul class="nav">
-          <li><a href="<?php echo url_for('default/error404') ?>">Test 404</a></li>
-          <li><a href="<?php echo url_for('default/disabled') ?>">Test disabled</a></li>
-          <li><a href="<?php echo url_for('default/login') ?>">Test login</a></li>
-          <li><a href="<?php echo url_for('default/module') ?>">Test module</a></li>
-          <li><a href="<?php echo url_for('default/secure') ?>">Test secure</a></li>
-        </ul>
-
-        <ul class="nav pull-right"><?php if ($sf_user->isAuthenticated()): ?>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <?php echo $sf_user->getUsername() ?>
-              <b class="caret"></b>
-            </a>
-            <ul class="dropdown-menu">
-              <li><a href="<?php echo url_for('sf_guard_signout') ?>">Log out</a></li>
-            </ul>
-          </li>
-        </ul><?php endif ?>
+      <div class="login-helpers">
+        <a href="" class="btn btn-facebook">Войти</a>
+        <a href="" class="btn btn-vk">Войти</a>
+        <a href="#" class="btn btn-circle"><i class="icon icon-plus"></i></a>
       </div>
+
+      <a href="#" class="btn-closer">×</a>
+    </div>
+
+    <div class="choose-city-bar hide">
+      <ul class="unstyled">
+        <li><a href="">Москва</a></li>
+        <li><a href="">Санкт-Петербург</a></li>
+        <li><a href="">Архангельск</a></li>
+        <li><a href="">Астрахань</a></li>
+        <li><a href="">Барнаул</a></li>
+      </ul>
+    </div>
+
+    <div class="head-nav">
+      <ul class="nav nav-pills">
+        <li class="active"><a href="">Текущие акции</a></li>
+        <li><a href="">Прошедшие акции</a></li>
+      </ul>
+
+      <div class="mega-basket">
+        <a href="" class="city-chooser">Владивосток</a>
+        <a href="<?php echo url_for('@homepage') ?>" class="logo">Cheapon</a>
+        <a href="" class="basket">
+          <span class="counter">0</span>
+        </a>
+      </div>
+
+      <ul class="nav nav-pills">
+        <li><a href="">Как это работает</a></li>
+        <li><a href="">Для бизнеса</a></li>
+      </ul>
     </div>
   </div>
 
-  <section id="content">
-    <div class="container">
-      <?php echo $sf_content ?>
-    </div>
-  </section>
+  <div id="content" class="container">
+    <?php echo $sf_content ?>
+  </div>
 
-  <footer id="footer">
-    <div class="container" style="padding-top: 7em">
-      Powered by <a href="http://github.com/b1rdex/symfony1">Symfony 1.4 "slowpoke" edition</a>
+  <div id="footer" class="container">
+    <div class="striped-head text-center">
+      <div class="subhead muted">Контактный телефон</div>
+      <div class="head">+7<span class="bullet"></span>914<span class="bullet"></span>961<span class="bullet"></span>96<span class="bullet"></span>98</div>
     </div>
-  </footer>
+
+    <div class="row footer-navs">
+      <ul class="span6 nav nav-list" style="padding-left:0; padding-right:0">
+        <li class="nav-header">Что такое «CHEAPON»</li>
+        <li><a href="">Как это работает</a></li>
+        <li><a href="">О компании</a></li>
+        <li><a href="">Вакансии</a></li>
+        <li><a href="">Контакты и реквизиты</a></li>
+      </ul>
+
+      <ul class="span6 nav nav-list" style="padding-left:0; padding-right:0">
+        <li class="nav-header">Для бизнеса</li>
+        <li><a href="">Привлечение клиентов</a></li>
+        <li><a href="">Предложить свое заведение</a></li>
+      </ul>
+
+      <ul class="span6 nav nav-list" style="padding-left:0; padding-right:0">
+        <li class="nav-header">Для клиентов</li>
+        <li><a href="">Вопросы и ответы</a></li>
+        <li><a href="">Пользовательское соглашение</a></li>
+      </ul>
+    </div>
+
+    <div class="row footer-last">
+      <div class="span6 text-left who-we-are muted">
+        © ООО «Cheapon», <?php echo date('Y') ?>  <br />
+        Скидки в Чите, товары и услуги со скидками
+      </div>
+
+      <div class="span6 text-center muted">
+        visa
+        mastercard
+        yabablo
+        webmanya
+      </div>
+
+      <div class="span6 text-right follow-the-white-rabbit muted">
+        Следите за акциями:
+        <a href="" class="btn bnt-circle"><i class="icon icon-vk"></i></a>
+        <a href="" class="btn bnt-circle"><i class="icon icon-fb"></i></a>
+        <a href="" class="btn bnt-circle"><i class="icon icon-tw"></i></a>
+        <a href="" class="btn bnt-circle"><i class="icon icon-plus"></i></a>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
