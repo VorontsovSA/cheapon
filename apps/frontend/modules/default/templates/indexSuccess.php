b11d40498784ca6c6fe814fc59fe3ad934567c08
Anatoly Pashin <anatoly.pashin@gmail.com>
2013-05-27 22:07:38 +1100

    makeup updates

--- apps/frontend/modules/default/templates/indexSuccess.php
+++ apps/frontend/modules/default/templates/indexSuccess.php
@@ -13,72 +13,91 @@
 
 <div class="mega-cells">
   <div class="row">
-    <div class="span6">
-      <div class="description">
-        <div class="description-full">
-          <div class="name">Механическая и ультразвуковая чистка лица, омолаживающий массаж лица с альгинатной фруктовой маской, 4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
-          <div class="counters">
-            <span class="eta">3 дня 23:12</span>
-            <span class="comments">234</span>
-            <span class="likes">71</span>
+    <div class="cell span6">
+      <div class="cell-image" style="background-image: url(/img/cells/1.png)">
+        <div class="description">
+          <div class="description-full">
+            <div class="name">Механическая и ультразвуковая чистка лица, омолаживающий массаж лица с альгинатной фруктовой маской, 4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
+            <div class="counters">
+              <span class="eta">3 дня 23:12</span>
+              <span class="comments">234</span>
+              <span class="likes">71</span>
+            </div>
+          </div>
+
+          <div class="description-short hide">
+            <div class="name">4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
           </div>
         </div>
 
-        <div class="description-short">
-          <div class="name">4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
+        <div class="buy-me-faster">
+          <div class="discount">
+            70<span class="percent">%</span>
+          </div>
+          <div class="price">
+            50<span class="currency">руб.</span>
+          </div>
+          <a href="" class="btn buy">Купить</a>
         </div>
-      </div>
-
-      <div class="buy-me-faster">
-        право на скидку 70%
-        50 руб.
-        <a href="" class="btn">Купить</a>
       </div>
     </div>
 
-    <div class="span6">
-      <div class="description">
-        <div class="description-full">
-          <div class="name">Механическая и ультразвуковая чистка лица, омолаживающий массаж лица с альгинатной фруктовой маской, 4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
-          <div class="counters">
-            <span class="eta">3 дня 23:12</span>
-            <span class="comments">234</span>
-            <span class="likes">71</span>
+    <div class="cell span6">
+      <div class="cell-image" style="background-image: url(/img/cells/2.png)">
+        <div class="description">
+          <div class="description-full">
+            <div class="name">Комплексный уход за лицом и телом
+в салоне красоты «Багира»</div>
+            <div class="counters">
+              <span class="eta">3 дня 23:12</span>
+              <span class="comments">234</span>
+              <span class="likes">71</span>
+            </div>
+          </div>
+
+          <div class="description-short hide">
+            <div class="name">4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
           </div>
         </div>
 
-        <div class="description-short">
-          <div class="name">4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
+        <div class="buy-me-faster">
+          <div class="discount">
+            70<span class="percent">%</span>
+          </div>
+          <div class="price">
+            50<span class="currency">руб.</span>
+          </div>
+          <a href="" class="btn buy">Купить</a>
         </div>
-      </div>
-
-      <div class="buy-me-faster">
-        право на скидку 70%
-        50 руб.
-        <a href="" class="btn">Купить</a>
       </div>
     </div>
 
-    <div class="span6">
-      <div class="description">
-        <div class="description-full">
-          <div class="name">Механическая и ультразвуковая чистка лица, омолаживающий массаж лица с альгинатной фруктовой маской, 4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
-          <div class="counters">
-            <span class="eta">3 дня 23:12</span>
-            <span class="comments">234</span>
-            <span class="likes">71</span>
+    <div class="cell span6">
+      <div class="cell-image" style="background-image: url(/img/cells/3.png)">
+        <div class="description">
+          <div class="description-full hide">
+            <div class="name">Механическая и ультразвуковая чистка лица, омолаживающий массаж лица с альгинатной фруктовой маской, 4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
+            <div class="counters">
+              <span class="eta">3 дня 23:12</span>
+              <span class="comments">234</span>
+              <span class="likes">71</span>
+            </div>
+          </div>
+
+          <div class="description-short">
+            <div class="name">4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
           </div>
         </div>
 
-        <div class="description-short">
-          <div class="name">4-х кислотный пилинг или нано-пилинг, мезотерапию лица или лимфодренажный массаж тела в салоне красоты «Багира»</div>
+        <div class="buy-me-faster">
+          <div class="discount">
+            70<span class="percent">%</span>
+          </div>
+          <div class="price">
+            50<span class="currency">руб.</span>
+          </div>
+          <a href="" class="btn buy">Купить</a>
         </div>
-      </div>
-
-      <div class="buy-me-faster">
-        право на скидку 70%
-        50 руб.
-        <a href="" class="btn">Купить</a>
       </div>
     </div>
   </div>
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -7,6 +7,7 @@
   <?php include_title() ?>
   <link rel="shortcut icon" href="/favicon.ico" />
   <?php include_stylesheets() ?>
+  <link href='http://fonts.googleapis.com/css?family=PT+Sans:700&subset=cyrillic' rel='stylesheet' type='text/css'>
   <?php include_javascripts() ?>
   <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -14,106 +15,122 @@
 </head>
 
 <body>
-  <div id="header" class="container">
-    <div class="login-bar hide">
-      <form action="" class="form-inline">
-        <input type="email" name="" id="" placeholder="Эл. почта" />
-        <input type="password" name="" id="" placeholder="Пароль" />
-        <button type="submit" class="btn">Войти</button>
-        <a href="">Регистрация</a>
-      </form>
+  <div class="all-the-headers">
+    <div id="login-bar" class="hide">
+      <div class="container">
+        <form action="" class="form-inline">
+          <input type="email" name="" id="" placeholder="Эл. почта" />
+          <input type="password" name="" id="" placeholder="Пароль" />
+          <button type="submit" class="btn">Войти</button>
+          <a href="">Регистрация</a>
+        </form>
 
-      <div class="login-helpers">
-        <a href="" class="btn btn-facebook">Войти</a>
-        <a href="" class="btn btn-vk">Войти</a>
-        <a href="#" class="btn btn-circle"><i class="icon icon-plus"></i></a>
+        <div class="login-helpers">
+          <a href="" class="btn btn-facebook">Войти</a>
+          <a href="" class="btn btn-vk">Войти</a>
+          <a href="#" class="btn btn-circle"><i class="icon icon-plus"></i></a>
+        </div>
+
+        <a href="#" class="btn-closer">×</a>
       </div>
-
-      <a href="#" class="btn-closer">×</a>
     </div>
 
-    <div class="choose-city-bar hide">
-      <ul class="unstyled">
-        <li><a href="">Москва</a></li>
-        <li><a href="">Санкт-Петербург</a></li>
-        <li><a href="">Архангельск</a></li>
-        <li><a href="">Астрахань</a></li>
-        <li><a href="">Барнаул</a></li>
-      </ul>
+    <div id="choose-city-bar" class="hide">
+      <div class="container">
+        <ul class="unstyled">
+          <li><a href="">Москва</a></li>
+          <li><a href="">Санкт-Петербург</a></li>
+          <li><a href="">Архангельск</a></li>
+          <li><a href="">Астрахань</a></li>
+          <li><a href="">Барнаул</a></li>
+        </ul>
+      </div>
     </div>
 
-    <div class="head-nav">
-      <ul class="nav nav-pills">
-        <li class="active"><a href="">Текущие акции</a></li>
-        <li><a href="">Прошедшие акции</a></li>
-      </ul>
+    <div id="header">
+      <div class="container">
+        <div class="head-nav clearfix">
+          <div class="pull-left">
+            <ul class="nav nav-pills">
+              <li class="active"><a href="">Текущие акции</a></li>
+              <li><a href="">Прошедшие акции</a></li>
+            </ul>
+          </div>
 
-      <div class="mega-basket">
-        <a href="" class="city-chooser">Владивосток</a>
-        <a href="<?php echo url_for('@homepage') ?>" class="logo">Cheapon</a>
-        <a href="" class="basket">
-          <span class="counter">0</span>
-        </a>
+          <div class="mega-basket">
+            <a href="" class="city-chooser">Владивосток</a>
+            <a href="<?php echo url_for('@homepage') ?>" class="logo hide-text" title="На главную страницу">Cheapon</a>
+            <a href="" class="basket" onclick="$(this).find('.counter').text(Math.round(Math.random()*10+3)); return false">
+              <span class="counter img-circle">0</span>
+            </a>
+          </div>
+
+          <div class="pull-right">
+            <ul class="nav nav-pills">
+              <li><a href="">Как это работает</a></li>
+              <li><a href="">Для бизнеса</a></li>
+              <li><a href="" class="btn m0">Вход</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
-
-      <ul class="nav nav-pills">
-        <li><a href="">Как это работает</a></li>
-        <li><a href="">Для бизнеса</a></li>
-      </ul>
     </div>
   </div>
 
-  <div id="content" class="container">
-    <?php echo $sf_content ?>
+  <div id="content">
+    <div class="container">
+      <?php echo $sf_content ?>
+    </div>
   </div>
 
-  <div id="footer" class="container">
-    <div class="striped-head text-center">
-      <div class="subhead muted">Контактный телефон</div>
-      <div class="head">+7<span class="bullet"></span>914<span class="bullet"></span>961<span class="bullet"></span>96<span class="bullet"></span>98</div>
-    </div>
-
-    <div class="row footer-navs">
-      <ul class="span6 nav nav-list" style="padding-left:0; padding-right:0">
-        <li class="nav-header">Что такое «CHEAPON»</li>
-        <li><a href="">Как это работает</a></li>
-        <li><a href="">О компании</a></li>
-        <li><a href="">Вакансии</a></li>
-        <li><a href="">Контакты и реквизиты</a></li>
-      </ul>
-
-      <ul class="span6 nav nav-list" style="padding-left:0; padding-right:0">
-        <li class="nav-header">Для бизнеса</li>
-        <li><a href="">Привлечение клиентов</a></li>
-        <li><a href="">Предложить свое заведение</a></li>
-      </ul>
-
-      <ul class="span6 nav nav-list" style="padding-left:0; padding-right:0">
-        <li class="nav-header">Для клиентов</li>
-        <li><a href="">Вопросы и ответы</a></li>
-        <li><a href="">Пользовательское соглашение</a></li>
-      </ul>
-    </div>
-
-    <div class="row footer-last">
-      <div class="span6 text-left who-we-are muted">
-        © ООО «Cheapon», <?php echo date('Y') ?>  <br />
-        Скидки в Чите, товары и услуги со скидками
+  <div id="footer">
+    <div class="container">
+      <div class="striped-head">
+        <h1 class="head">
+          <span>+7<span class="bullet"></span>914<span class="bullet"></span>961<span class="bullet"></span>96<span class="bullet"></span>98</span>
+          <div class="subhead muted">Контактный телефон</div>
+        </h1>
       </div>
 
-      <div class="span6 text-center muted">
-        visa
-        mastercard
-        yabablo
-        webmanya
+      <div class="row footer-navs">
+        <ul class="span6 nav nav-list">
+          <li class="nav-header">Что такое «CHEAPON»</li>
+          <li><a href="">Как это работает</a></li>
+          <li><a href="">О компании</a></li>
+          <li><a href="">Вакансии</a></li>
+          <li><a href="">Контакты и реквизиты</a></li>
+        </ul>
+
+        <ul class="span6 nav nav-list">
+          <li class="nav-header">Для бизнеса</li>
+          <li><a href="">Привлечение клиентов</a></li>
+          <li><a href="">Предложить свое заведение</a></li>
+        </ul>
+
+        <ul class="span6 nav nav-list">
+          <li class="nav-header">Для клиентов</li>
+          <li><a href="">Вопросы и ответы</a></li>
+          <li><a href="">Пользовательское соглашение</a></li>
+        </ul>
       </div>
 
-      <div class="span6 text-right follow-the-white-rabbit muted">
-        Следите за акциями:
-        <a href="" class="btn bnt-circle"><i class="icon icon-vk"></i></a>
-        <a href="" class="btn bnt-circle"><i class="icon icon-fb"></i></a>
-        <a href="" class="btn bnt-circle"><i class="icon icon-tw"></i></a>
-        <a href="" class="btn bnt-circle"><i class="icon icon-plus"></i></a>
+      <div class="row footer-last">
+        <div class="span6 text-left who-we-are muted">
+          © ООО «Cheapon», <?php echo date('Y') ?>  <br />
+          Скидки в Чите, товары и услуги со скидками
+        </div>
+
+        <div class="span6 text-center money-we-accept muted">
+          <img src="/img/money-we-accept.png" alt="visa, mastercard, yandex.money, webmoney" />
+        </div>
+
+        <div class="span6 text-right follow-the-white-rabbit muted">
+          Следите за акциями:
+          <a href="" class="btn bnt-circle"><i class="icon icon-vk"></i></a>
+          <a href="" class="btn bnt-circle"><i class="icon icon-fb"></i></a>
+          <a href="" class="btn bnt-circle"><i class="icon icon-tw"></i></a>
+          <a href="" class="btn bnt-circle"><i class="icon icon-plus"></i></a>
+        </div>
       </div>
     </div>
   </div>
