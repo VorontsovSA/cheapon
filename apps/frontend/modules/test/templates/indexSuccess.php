5196b7e8a5caf0fe0ca5aaf70c63ec66d315b0bf
Anatoly Pashin <anatoly.pashin@gmail.com>
2013-06-26 01:58:17 +1100

    partial event page

--- apps/frontend/modules/test/templates/indexSuccess.php
+++ apps/frontend/modules/test/templates/indexSuccess.php
@@ -7,11 +7,40 @@
   <h5>Купон действует с 30 августа по 15 сентября</h5>
 </div>
 
-<div class="mega-border page">
-  <div class="striped-head">
-    <h2>Условия</h2>
+<div class="mega-border page with-grafon-zbs">
+  <div class="grafon-zbs" style="background-image: url(http://lorempixel.com/375/290/?1)">
+    <div class="second-bg" style="background-image: url(http://lorempixel.com/375/290/?2)"></div>
+    <div class="take-the-last">
+      <span>Осталось 3 купона!</span>
+    </div>
+    <div class="centrota">
+      <div class="discount">
+        70<span class="percent">%</span>
+      </div>
+      <div class="price">
+        50<span class="currency">руб.</span>
+      </div>
+      <div class="buy">
+        <a href="" class="btn">Купить купон</a>
+      </div>
+    </div>
+    <div class="nizota">
+      <div class="eta">
+        3 дня 23:12
+      </div>
+      <div class="comments">
+        234
+      </div>
+      <div class="likes">
+        890
+      </div>
+    </div>
   </div>
-  <div class="row">
+
+  <div class="striped-head">
+    <h3>Условия</h3>
+  </div>
+  <div class="row terms-list">
     <div class="span8">
       <ul>
         <li>Купоны действительны с 16 мая по 16 июля 2013 года;</li>
@@ -34,7 +63,7 @@
   </div>
 
   <div class="striped-head">
-    <h2>Описание</h2>
+    <h3>Описание</h3>
   </div>
   <div class="text-center">
     <p>Ультразвуковая чистка – один из самых бережных и в то же время эффективных способов глубокого очищения, благодаря которому кожа становится более гладкой и действительно чистой, а цвет лица ровным. Такая процедура занимает немного времени и не требует специальной подготовки перед ее проведением.</p>
@@ -44,7 +73,7 @@
   </div>
 
   <div class="striped-head">
-    <h2>Ресторан «Русская охота»</h2>
+    <h3>Ресторан «Русская охота»</h3>
   </div>
   <div class="text-center">
     <p>Ресторан «Русская охота» открывает двери для всех ценителей вкусной и здоровой пищи, для тех, кому важна уютная атмосфера и внимательное обслуживание персонала.</p>
@@ -55,7 +84,7 @@
   <h1 class="text-center" style="color:red">Контактная информация</h1>
 
   <div class="striped-head">
-    <h2>Комментарии</h2>
+    <h3>Комментарии</h3>
   </div>
   <ul class="media-list">
     <li class="media">
@@ -63,19 +92,23 @@
         <img class="media-object" src="http://lorempixel.com/64/64/?1">
       </a>
       <div class="media-body">
-        <h4 class="media-heading">Alexander Nevmerejskiy, 22 мая 2013  32:48</h4>
+        <h4 class="media-heading muted">Alexander Nevmerejskiy, 22 мая 2013  32:48</h4>
         <p>Открывает двери для всех ценителей вкусной и здоровой пищи, для тех, кому важна уютная атмосфера и внимательное обслуживание персонала?</p>
 
-        <div class="media">
+        <div class="media well">
           <a class="pull-left" href="#">
             <img class="media-object" src="http://lorempixel.com/64/64/?2">
           </a>
           <div class="media-body">
-            <h4 class="media-heading">Cheapon, 22 мая 2013  32:48</h4>
+            <h4 class="media-heading muted">Cheapon, 22 мая 2013  32:48</h4>
             <p>Да! Двери для всех ценителей вкусной и здоровой пищи, для тех, кому важна уютная атмосфера и внимательное обслуживание персонала!</p>
           </div>
        </div>
       </div>
     </li>
   </ul>
+  <form class="text-center">
+    <textarea name="" id="" cols="30" rows="10">Ваш комментарий…</textarea><br />
+    <button type="submit" class="btn">Добавить комментарий</button>
+  </form>
 </div>
