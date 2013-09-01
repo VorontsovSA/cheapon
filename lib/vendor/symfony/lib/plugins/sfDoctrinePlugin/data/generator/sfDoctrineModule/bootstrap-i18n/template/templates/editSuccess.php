6f0585eca3a492aaf41536f14a92355c6c0ebd60
Anatoly Pashin <anatoly.pashin@gmail.com>
2013-09-01 22:27:53 +1100

    replaced symfony

--- lib/vendor/symfony/lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineModule/bootstrap-i18n/template/templates/editSuccess.php
+++
@@ -1,5 +0,0 @@
-<h1 class="page-header">
-  [?php echo __('Edit <?php echo sfInflector::humanize($this->getSingularName()) ?>') ?]
-</h1>
-
-[?php include_partial('form', array('form' => $form)) ?]
--- lib/vendor/symfony/lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineModule/bootstrap/template/templates/newSuccess.php
+++
@@ -1,5 +0,0 @@
-<h1 class="page-header">
-  New <?php echo sfInflector::humanize($this->getSingularName()) . "\n" ?>
-</h1>
-
-[?php include_partial('form', array('form' => $form)) ?]
